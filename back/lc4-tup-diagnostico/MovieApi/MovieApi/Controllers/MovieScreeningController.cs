51a3cc2f6878582e61d4ea81cf93ce799f261205
ManueldeMacedo <75130351+ManueldeMacedo@users.noreply.github.com>
2024-08-07 21:42:42 -0300

    First version of Movie API

---
+++ back/lc4-tup-diagnostico/MovieApi/MovieApi/Controllers/MovieScreeningController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieScreeningController : ControllerBase
+    {
+    }
+}
