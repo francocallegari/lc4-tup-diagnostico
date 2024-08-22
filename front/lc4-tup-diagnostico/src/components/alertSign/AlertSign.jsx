20340ac51499b94be68f8a37bd8ea571367db2a3
francocallegari <francocallegari12@gmail.com>
2024-08-22 20:17:11 -0300

    created alert message and css

--- front/lc4-tup-diagnostico/src/components/alertSign/AlertSign.jsx
+++ front/lc4-tup-diagnostico/src/components/alertSign/AlertSign.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Alert from "react-bootstrap/Alert";
+import "./alertSign.css"; // Importa el archivo CSS
 
-const AlertSign = () => {
+const AlertSign = ({ message, variant = "primary", duration = 5000 }) => {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setShow(false), duration);
+  }, [duration]);
+
   return (
-    <div>AlertSign</div>
-  )
-}
+    <>
+      {show && (
+        <div className="alert-container">
+          <Alert variant={variant}>{message}</Alert>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default AlertSign
+export default AlertSign;
