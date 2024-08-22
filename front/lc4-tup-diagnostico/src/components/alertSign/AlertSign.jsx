82ab2885bcc8c654f66d27af834aaf64bb56a954
francocallegari <francocallegari12@gmail.com>
2024-08-22 20:18:17 -0300

    alert css

--- front/lc4-tup-diagnostico/src/components/alertSign/AlertSign.jsx
+++ front/lc4-tup-diagnostico/src/components/alertSign/AlertSign.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Alert from "react-bootstrap/Alert";
-import "./alertSign.css"; // Importa el archivo CSS
+import "./alertSign.css"; 
 
 const AlertSign = ({ message, variant = "primary", duration = 5000 }) => {
   const [show, setShow] = useState(true);
