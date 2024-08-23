d884fad551803821500ccbc004b5839f3c3bfa70
AnaRustici <anabellarustici@gmail.com>
2024-08-23 15:42:43 -0300

    added validations on Create method

--- back/lc4-tup-diagnostico/MovieApi/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ back/lc4-tup-diagnostico/MovieApi/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72e518ffaacf2fdbb3a9647865c7a94955dfc2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ab2885bcc8c654f66d27af834aaf64bb56a954")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
