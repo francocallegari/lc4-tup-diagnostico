3d0adf1f842ee05f2b27ecff04f25d8194ca8bf7
ManueldeMacedo <75130351+ManueldeMacedo@users.noreply.github.com>
2024-08-23 20:01:52 -0300

    Se agregaron alertas para mostrar los mensajes de error

--- front/lc4-tup-diagnostico/src/components/alertSign/AlertSign.jsx
+++ front/lc4-tup-diagnostico/src/components/alertSign/AlertSign.jsx
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Alert from "react-bootstrap/Alert";
-import "./alertSign.css"; 
+import "./alertSign.css";
 
-const AlertSign = ({ message, variant = "primary", duration = 5000 }) => {
-  const [show, setShow] = useState(true);
+const AlertSign = ({ message, variant = "primary", duration = 5000, onClose }) => {
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setShow(false), duration);
-  }, [duration]);
+    if (message) {
+      setShow(true);
+      const timer = setTimeout(() => {
+        setShow(false);
+        onClose();
+      }, duration);
+      return () => clearTimeout(timer);
+    }
+  }, [message, duration, onClose]);
 
   return (
     <>
       {show && (
         <div className="alert-container">
-          <Alert variant={variant}>{message}</Alert>
+          <Alert variant={variant} onClose={() => {
+            setShow(false);
+            onClose();
+          }} dismissible>
+            {message}
+          </Alert>
         </div>
       )}
     </>
--- front/lc4-tup-diagnostico/src/components/functionContainer/FunctionContainer.jsx
+++ front/lc4-tup-diagnostico/src/components/functionContainer/FunctionContainer.jsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import FunctionList from "../functionList/FunctionList";
 import FunctionModal from "../functionModal/FunctionModal";
 import EditFunctionModal from "../editFunctionModal/EditFunctionModal";
+import AlertSign from "../alertSign/AlertSign";
 
 const FunctionContainer = () => {
   const [showModal, setShowModal] = useState(false);
@@ -10,6 +11,7 @@ const FunctionContainer = () => {
   const [peliculas, setPeliculas] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [errorMsj, setErrorMsj] = useState("");
   const [showEditModal, setShowEditModal] = useState(false);
   const [functionToEdit, setFunctionToEdit] = useState(null);
 
@@ -25,13 +27,26 @@ const FunctionContainer = () => {
         const peliculaResponse = await fetch(
           "https://localhost:7062/api/Pelicula/Peliculas"
         );
+
+        if (!functionResponse.ok) {
+          const errorMessage = await response.text();
+          setErrorMsj(errorMessage);
+          return;
+        }
+
+        if (!peliculaResponse.ok) {
+          const errorMessage = await response.text();
+          setErrorMsj(errorMessage);
+          return;
+        }
+
         const peliculaData = await peliculaResponse.json();
         setPeliculas(peliculaData);
-
         setLoading(false);
       } catch (err) {
         setError("Failed to fetch data");
         setLoading(false);
+        setErrorMsj("Erro al cargar la información");
       }
     };
 
@@ -49,7 +64,9 @@ const FunctionContainer = () => {
       });
 
       if (!response.ok) {
-        throw new Error("Failed to add function");
+        const errorMessage = await response.text();
+        setErrorMsj(errorMessage);
+        return;
       }
 
       const addedFunction = await response.json();
@@ -67,7 +84,9 @@ const FunctionContainer = () => {
       });
 
       if (!response.ok) {
-        throw new Error("Failed to delete function");
+        const errorMessage = await response.text();
+        setErrorMsj(errorMessage);
+        return;
       }
 
       setFunctions((prevFunctions) =>
@@ -92,7 +111,9 @@ const FunctionContainer = () => {
       );
   
       if (!response.ok) {
-        throw new Error("Failed to update function");
+        const errorMessage = await response.text();
+        setErrorMsj(errorMessage);
+        return;
       }
   
       const editedFunction = await response.json();
@@ -114,11 +135,16 @@ const FunctionContainer = () => {
     setShowEditModal(true);
   };
 
+  const handleAlertClose = () => {
+    setErrorMsj("");
+  };
+
   return (
     <div>
       <h2>Funciones:</h2>
       {loading && <p>Cargando la información...</p>}
       {error && <p>Error: {error}</p>}
+      {errorMsj && <AlertSign message={errorMsj} variant="danger" duration={5000} onClose={handleAlertClose}/>}
       {functions && (
         <FunctionList
           functions={functions}
